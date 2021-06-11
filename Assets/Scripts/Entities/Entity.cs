3e1e37f05e04864e3500f3c940701150ade5a3a2
Kryxzael <kasper.ramskjell98@gmail.com>
2021-06-11 23:18:10 +0200

    Add entity base class

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Anything that can be placed on a tile
+/// </summary>
+public class Entity
+{
+}
