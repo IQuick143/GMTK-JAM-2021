249c0cc3cc1ca18f06c2efdf2d2e08f6a4b40162
Kryxzael <kasper.ramskjell98@gmail.com>
2021-06-12 18:20:55 +0200

    Maybe better camera (please rebase or something)

--- Assets/Scripts/MoveCameraWithMouse.cs
+++ Assets/Scripts/MoveCameraWithMouse.cs
@@ -8,29 +8,38 @@ using UnityEngine;
 
 public class MoveCameraWithMouse : MonoBehaviour
 {
-	private Vector3 _lastMousePosition;
+	private Vector3 _clickedWorldPosition;
+	private Vector3 _clickedCameraPosition;
+	private Camera _cam;
 
 	public float HorizontalSpeed = 0.25f;
 	public float VerticalSpeed = 0.25f;
+
+	private void Awake()
+	{
+		_cam = Camera.main;
+	}
 
 	private void Update()
 	{
 		//TODO: Restrict to bounds of map
 
 		if (Input.GetMouseButtonDown(0))
-			_lastMousePosition = Input.mousePosition;
-
+		{
+			_clickedWorldPosition = GameManager.input.GetMouseXZIntersect();
+			_clickedCameraPosition = transform.position;
+		}
 		else if (Input.GetMouseButton(0))
 		{
-			var newMousePosition = Input.mousePosition;
 
-			transform.position += new Vector3(
-				x: (_lastMousePosition.x - newMousePosition.x) * HorizontalSpeed, 
-				y: 0f, 
-				z: (_lastMousePosition.y - newMousePosition.y) * VerticalSpeed
+
+			var newWorldPosition = GameManager.input.GetMouseXZIntersect();
+
+			transform.position = _clickedWorldPosition - new Vector3(
+				x: (_clickedWorldPosition.x - newWorldPosition.x) * HorizontalSpeed,
+				y: 0f,
+				z: (_clickedWorldPosition.y - newWorldPosition.y) * VerticalSpeed
 			);
-
-			_lastMousePosition = newMousePosition;
 		}
 	}
 }
