c5a6ec56143ad4c0e8646303dfea3c420e576a92
IQuick 143 <IQuick143cz@gmail.com>
2021-06-13 13:47:58 +0200

    Add Wire colouring

--- Assets/Scripts/Entities/Wire.cs
+++ Assets/Scripts/Entities/Wire.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Wire : Entity {
 	private Connection connection;
 	private HexDirection direction;
+	[SerializeField]
+	private List<Renderer> renderers;
 
 	public void SetConnection(Connection connection, HexDirection direction) {
 		this.connection = connection;
 		this.direction = direction;
 		this.transform.rotation = Quaternion.AngleAxis(direction.ToDegrees(), Vector3.up);
+		foreach (var renderer in renderers) {
+			renderer.material.color = connection.Stream.GetColour();
+		}
 	}
 
 	void OnDestroy() {
