35200dec111b9f63bffdfe32457af9c2c6f097c4
Kryxzael <kasper.ramskjell98@gmail.com>
2021-06-13 17:20:41 +0200

    Add a ridiculously bad title screen

---
+++ Assets/Scripts/Meta/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public void StartGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void EndGame()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+	}
+}
